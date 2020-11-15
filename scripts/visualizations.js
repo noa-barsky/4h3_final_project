8e431dfdc9a1ab665cc943b5cd09292dc3dd4ec0
connorhewick <connor@hewick.ca>
2020-11-15 18:50:39 -0500

    add hardcoded graphs with show/hide button

--- scripts/visualizations.js
+++ scripts/visualizations.js
@@ -1,33 +1,80 @@
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawInVSExp);
-function drawInVSExp() {
-  var data = google.visualization.arrayToDataTable([
-    ['Language', 'Speakers (in millions)'],
-    ['German',  5.85],
-    ['French',  1.66],
-    ['Italian', 0.316],
-    ['Romansh', 0.0791]
-  ]);
+google.charts.setOnLoadCallback(drawExpBreakdown);
+google.charts.setOnLoadCallback(drawIncomeBreakdown);
 
-  var options = {
-    legend: 'none',
-    pieSliceText: 'label',
-    title: 'Swiss Language Use (100 degree rotation)',
-    pieStartAngle: 100,
-  };
-
-  var chart = new google.visualization.PieChart(document.getElementById('inVSexp'));
-  chart.draw(data, options);
-}
 
 function showChart(chart, button) {
   var chart = document.getElementById(chart);
   var button = document.getElementById(button);
-  if (chart.style.visibility == "hidden") {
-    chart.style.visibility = "visible";
+  if (chart.style.display == "none") {
+    chart.style.display = "inline-block";
     button.innerHTML = "Hide";
   } else {
-    chart.style.visibility = "hidden";
+    chart.style.display = "none";
     button.innerHTML = "Show";
   }
+}
+
+function drawInVSExp() {
+  var data = google.visualization.arrayToDataTable([
+    ['Item', 'Amount (in dollars)'],
+    ['Income',  700+700+80],
+    ['Expenses',  85+275+450]
+  ]);
+  var options = {
+    legend: 'none',
+    pieSliceText: 'label',
+    pieStartAngle: 100,
+    slices: {
+            0: { color: 'green' },
+            1: { color: 'red' }
+          },
+    pieSliceTextStyle: {
+            color: 'black',
+          }
+  };
+  var chart = new google.visualization.PieChart(document.getElementById('inVSexp'));
+  chart.draw(data, options);
+}
+
+function drawIncomeBreakdown() {
+  var data = google.visualization.arrayToDataTable([
+    ['Item', 'Amount (in dollars)'],
+    ['Salary',  275],
+    ['Textbook Sale',  85]
+  ]);
+  var options = {
+    legend: 'none',
+    pieSliceText: 'label',
+    pieStartAngle: 100,
+    pieSliceTextStyle: {
+            color: 'black',
+          },
+    slices: {
+            0: { color: 'green' },
+            1: { color: 'blue' }
+          },
+  };
+  var chart = new google.visualization.PieChart(document.getElementById('incomeBreakdown'));
+  chart.draw(data, options);
+}
+
+function drawExpBreakdown() {
+  var data = google.visualization.arrayToDataTable([
+    ['Item', 'Amount (in dollars)'],
+    ['Phone Bill',  85],
+    ['Car Insurance',  275],
+    ['Rent', 450]
+  ]);
+  var options = {
+    legend: 'none',
+    pieSliceText: 'label',
+    pieStartAngle: 100,
+    pieSliceTextStyle: {
+            color: 'black',
+          }
+  };
+  var chart = new google.visualization.PieChart(document.getElementById('expBreakdown'));
+  chart.draw(data, options);
 }
