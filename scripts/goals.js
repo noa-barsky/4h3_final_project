3f602dabccf89020f4eb8888a5eeabb373b22fe2
noa-barsky <barskyn@mcmaster.ca>
2020-11-14 22:48:21 -0500

    Can now add goals when clicking add

---
+++ scripts/goals.js
@@ -0,0 +1,28 @@
+console.log("connected");
+function addGoal() {
+    let goalName = document.getElementById('goalInputField').value;
+    let saved = prompt("Target amount: ");
+    let target = prompt("Amount saved: ");
+    document.getElementById("goals").innerHTML += goalCardTemplate(goalName, saved, target);
+}
+
+// https://stackoverflow.com/questions/7060750/detect-the-enter-key-in-a-text-input-field
+const node = document.getElementById("goalInputField");
+node.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        addGoal();
+    }
+});
+
+function goalCardTemplate(title, saved, target) {
+    return `<div class="card">
+    <div class="card-body">
+        <h5 class="card-title" style="text-align: left;">
+            ${title}
+        </h5>
+      <p style="text-align: left;">Target: ${target}</p>
+      <p style="text-align: left;">Saved: ${saved}</p>
+    </div>
+  </div>
+  <br>`;
+}
