87a209602403e98c0b14c5a74d45321eba085dea
connorhewick <connor@hewick.ca>
2020-11-15 18:35:00 -0500

    prelim graphs

--- scripts/visualizations.js
+++ scripts/visualizations.js
@@ -1,22 +1,33 @@
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-
-function drawChart() {
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawInVSExp);
+function drawInVSExp() {
   var data = google.visualization.arrayToDataTable([
-    ['Year', 'Sales', 'Expenses'],
-    ['2004',  1000,      400],
-    ['2005',  1170,      460],
-    ['2006',  660,       1120],
-    ['2007',  1030,      540]
+    ['Language', 'Speakers (in millions)'],
+    ['German',  5.85],
+    ['French',  1.66],
+    ['Italian', 0.316],
+    ['Romansh', 0.0791]
   ]);
 
   var options = {
-    title: 'Company Performance',
-    curveType: 'function',
-    legend: { position: 'bottom' }
+    legend: 'none',
+    pieSliceText: 'label',
+    title: 'Swiss Language Use (100 degree rotation)',
+    pieStartAngle: 100,
   };
 
-  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
+  var chart = new google.visualization.PieChart(document.getElementById('inVSexp'));
   chart.draw(data, options);
+}
+
+function showChart(chart, button) {
+  var chart = document.getElementById(chart);
+  var button = document.getElementById(button);
+  if (chart.style.visibility == "hidden") {
+    chart.style.visibility = "visible";
+    button.innerHTML = "Hide";
+  } else {
+    chart.style.visibility = "hidden";
+    button.innerHTML = "Show";
+  }
 }
